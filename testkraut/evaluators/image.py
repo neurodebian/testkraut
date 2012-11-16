183d80353d038d06955b43a34020d6f3e16cda4f
Michael Hanke <michael.hanke@gmail.com>
2012-11-16 13:57:57 +0100

    ENH: Avoid global dependency on nibabel

--- testkraut/evaluators/image.py
+++ testkraut/evaluators/image.py
@@ -1,6 +1,6 @@
-import nibabel as nb
-import numpy as np
 
 def image_nelements_positive(filepath):
+    import nibabel as nb
+    import numpy as np
     img = nb.load(filepath)
     return np.sum(img.get_data() > 0)
