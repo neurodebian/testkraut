1402958135520a41e3926649190f53827e3146e7
Michael Hanke <michael.hanke@gmail.com>
2012-11-29 17:38:24 +0100

    ENH: Big update for fingerprinters

--- testkraut/fingerprinting.py
+++ testkraut/fingerprinting.py
@@ -13,11 +13,14 @@ __docformat__ = 'restructuredtext'
 import os
 import operator
 import logging
+import fileinput
 lgr = logging.getLogger(__name__)
 
 _tag2fx = {}
 
 def get_fingerprinters(tag):
+    """Return a sequence of fingerprint functors for a specific tag.
+    """
     if not len(_tag2fx):
         # charge
         from testkraut import cfg
@@ -58,6 +61,8 @@ def fp_file(fname, fp, tags):
         lgr.debug("no 'magic' package found -- cannot determine filemagic")
 
 def fp_volume_image(fname, fp, tags):
+    """Fingerprint for volumetric images
+    """
     # this version needs an increment whenever this implementation changes
     fp['__version__'] = 0
     import nibabel as nb
@@ -155,6 +160,11 @@ def fp_volume_image(fname, fp, tags):
                 cli['max'] = img_data[pos]
 
 def fp_nifti1_header(fname, fp, tags):
+    """Store the content of a NIfTI1 file header as fingerprint.
+
+    The fingerprint will also contain type codes and sizes for any header
+    extension found in a file.
+    """
     import nibabel as nb
     import numpy as np
     img = nb.load(fname)
@@ -173,21 +183,101 @@ def fp_nifti1_header(fname, fp, tags):
     fp['extension_codes'] = hdr.extensions.get_codes()
     fp['extension_sizes'] = [e.get_sizeondisk() for e in hdr.extensions]
 
-def fp_text_table(fname, fp, tags):
-    """Read entire text-based tables instead of computing a actual fingerprint
+def fp_numeric_values(fname, fp, tags):
+    """Basic fingerprint for matrices are arrays of numeric values.
 
-    Any table format dialect that is understood but the 'csv' module can be
-    read in full. Tables have to have a header with column names. These names
-    are used as fieldnames in the fingerprint structure. The actual coulmn data
-    is stored in full. However, an attempt is made to determine the dtype of
-    each column individually and convert the data accordingly. Only integer
-    values, floating point numbers and strings are distinguished.
+    For text files this function is trying to determine a potential comment
+    character before reading the file content is handed over to NumPy's
+    ``loadtxt()`` function.
+
+    Any data array will be described with basic statistics (mean, std, ...),
+    size of the matrix and data type. Files tagged 'columns' or 'rows' will
+    get per column or per row statistics respectively. Additionally, the
+    fingerprint for such files will also contain the average power spectrum.
     """
     fp['__version__'] = 0
+    import numpy as np
+    if 'text file' in tags:
+        first_chars = set()
+        fi = fileinput.FileInput(fname, openhook=fileinput.hook_compressed)
+        for line in fi:
+            first_chars.add(line[0])
+        comment_char = first_chars.difference(
+                [str(i) for i in range(10)] + ['\n', '\t', ' ', '.', '-'])
+        if len(comment_char) > 1:
+            raise ValueError("Cannot determine the comment character")
+        if len(comment_char):
+            comment_char = comment_char.pop()
+        else:
+            comment_char = None
+        if 'whitespace-separated fields' in tags:
+            delimiter=None
+        else:
+            delimiter=None
+        data = np.loadtxt(fname, comments=comment_char)
+    else:
+        # TODO load binary data
+        return
+    # basic info
+    fp['dtype'] = data.dtype.name
+    if len(data.shape):
+        fp['shape'] = data.shape
+    else:
+        fp['value'] = data.item()
+        # we cannot be more precise
+        return
+    from scipy.stats import describe
+    # what is the interesting axis
+    did_something = False
+    # a bit complicated to allow for description of rows and columns
+    for tag, axis in (('column', 0), ('row', 1)):
+        if not '%ss' % tag in tags:
+            if did_something:
+                continue
+            else:
+                # do stats on the whole thing
+                axis = None
+                tag = 'global'
+        did_something = True
+        # basic descriptive stats
+        _size, _minmax, _mean, _var, _skew, _kurt = describe(data, axis=axis)
+        fp['%s_std' % tag] = np.sqrt(_var)
+        fp['%s_mean' % tag] = _mean
+        fp['%s_min' % tag] = _minmax[0]
+        fp['%s_max' % tag] = _minmax[1]
+        fp['%s_skewness' % tag] = _skew
+        fp['%s_kurtosis' % tag] = _kurt
+        if not axis is None:
+            fp['%s_pwr_spectr' % tag] = \
+                    np.mean(np.abs(np.fft.fft(data, axis=axis))**2, axis=axis)
+
+
+def fp_table(fname, fp, tags):
+    """Read an entire table instead of computing a actual fingerprint
+
+    For text files, any table format dialect that is understood but the 'csv'
+    module can be read in full. Tables have to have a header with column names.
+    These names are used as fieldnames in the fingerprint structure. The actual
+    column data is stored in full. However, an attempt is made to determine the
+    dtype of each column individually and convert the data accordingly. Only
+    integer values, floating point numbers and strings are distinguished.
+    """
+    fp['__version__'] = 0
+    if 'text file' in tags:
+        _fp_text_table(fname, fp, tags)
+
+def _fp_text_table(fname, fp, tags):
     import csv
-    f = open(fname, 'r')
+    f = open(fname, 'rU')
     sniffer = csv.Sniffer()
-    dialect = sniffer.sniff(f.read(1024))
+    try:
+        dialect = sniffer.sniff(f.read(1024))
+    except:
+        # maybe a sloppy header with a trailing delimiter?
+        f.seek(0)
+        sample = [f.readline() for s in range(3)]
+        sample[0] = sample[0].strip()
+        dialect = sniffer.sniff('\n'.join(sample))
     f.seek(0)
     reader = csv.DictReader(f, dialect=dialect)
     fp.update(dict(zip(reader.fieldnames,
@@ -197,9 +287,13 @@ def fp_text_table(fname, fp, tags):
             fp[k].append(v)
     # improve dtypes -- do last do keep data when no numpy is around
     import numpy as np
+    del_me = []
     for k, v in fp.iteritems():
         if k.startswith('__') and k.endswith('__'):
             continue
+        if not len(k) and len(v) and v[0] is None:
+            # this is an artifact of a trailing delimiter
+            del_me.append(k)
         try:
             fp[k] = np.array(v, dtype=int)
         except ValueError:
@@ -208,3 +302,9 @@ def fp_text_table(fname, fp, tags):
             except ValueError:
                 # we tried ...
                 pass
+        except TypeError:
+            # tolerate any unexpected types and keep them as is
+            pass
+    for d in del_me:
+        # delete artifacts
+        del fp[d]
--- testkraut/tests/test_fingerprints.py
+++ testkraut/tests/test_fingerprints.py
@@ -20,10 +20,31 @@ def test_fingerprint_dict():
     vol_fp = get_fingerprinters('volumetric image')
     assert_true(len(vol_fp))
 
+@with_tempdir()
+def test_numeric_values_fp(wdir):
+    stuff="""/NumWaves	4
+/NumPoints	180
+/PPheights		1.000000e+00	7.177207e-01	1.000000e+00	7.177133e-01
+
+/Matrix
+8.868212e-02	3.147391e-02	-6.863332e-02	3.371175e-02	
+1.183776e-01	3.039748e-02	-3.464294e-02	3.336017e-02	
+"""
+    fname = opj(wdir, 'numeric')
+    f=open(fname, 'w')
+    f.write(stuff)
+    f.close()
+    fp = {}
+    # should auto-detect the comment character '/'
+    fp_numeric_values(fname, fp, ['text file', 'columns', 'rows'])
+    assert_equal(fp['shape'], (2, 4))
+    assert_equal(len(fp['column_mean']), 4)
+    assert_equal(len(fp['row_std']), 2)
+    assert_equal(len(fp['row_pwr_spectr']), 2)
 
 @with_tempdir()
 def test_table_fp(wdir):
-    ttable="""Cluster Index\tVoxels\tP\t-log10(P)\tZ-MAX\tZ-MAX X (vox)\tZ-MAX Y (vox)\tZ-MAX Z (vox)\tZ-COG X (vox)\tZ-COG Y (vox)\tZ-COG Z (vox)
+    ttable="""Cluster Index\tVoxels\tP\t-log10(P)\tZ-MAX\tZ-MAX X (vox)\tZ-MAX Y (vox)\tZ-MAX Z (vox)\tZ-COG X (vox)\tZ-COG Y (vox)\tZ-COG Z (vox)	
 a\t4134\t0\t149\t17.4\t46\t28\t8\t33.1\t23.6\t9.18
 b\t35\t0.000126\t3.9\t4.53\t37\t34\t17\t37.4\t34.3\t16.7
 
@@ -33,7 +54,7 @@ b\t35\t0.000126\t3.9\t4.53\t37\t34\t17\t37.4\t34.3\t16.7
     f.write(ttable)
     f.close()
     fp = {}
-    fp_text_table(table_name, fp, [])
+    fp_table(table_name, fp, ['text file'])
     assert_equal(len(fp), 12) # one for __version__
     assert_equal(type(fp['Cluster Index'][0]), str)
     assert_true(np.issubdtype(type(fp['Voxels'][1]), int))
