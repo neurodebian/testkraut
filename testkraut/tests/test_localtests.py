3c62d04e4672e607e7cda8f56a41a5d481a47353
Michael Hanke <michael.hanke@gmail.com>
2013-05-02 18:00:59 +0200

    TEST: Add an adapter to serve SPEC-based from a local library
    as part of the unit tests.

---
+++ testkraut/tests/test_localtests.py
@@ -0,0 +1,29 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the testkraut package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""Dog food!
+
+This is Testkraut's own unit test interface to serve the local SPECs within
+the unit tests battery as actual test cases.
+"""
+import logging
+import os.path as op
+
+lgr = logging.getLogger('testkraut')
+console = logging.StreamHandler()
+lgr.addHandler(console)
+lgr.setLevel(logging.DEBUG)
+
+from testkraut.testcase import TestFromSPEC, discover_specs, template_case, TemplateTestCase
+
+class LocalDogFoodTests(TestFromSPEC):
+    __metaclass__ = TemplateTestCase
+    @template_case(discover_specs([op.join(op.dirname(__file__),
+                                           'localtests')]))
+    def _run_spec_test(self, spec_filename):
+        return TestFromSPEC._run_spec_test(self, spec_filename)
