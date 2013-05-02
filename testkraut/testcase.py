fd77e00df0ccb67431627df38aef1b9ed21d94ba
Michael Hanke <michael.hanke@gmail.com>
2013-05-02 17:39:34 +0200

    Bulk commit to restart work

--- testkraut/testcase.py
+++ testkraut/testcase.py
@@ -12,22 +12,22 @@ __docformat__ = 'restructuredtext'
 
 import os
 from os.path import join as opj
-import json
+from json import dumps as jds
 from functools import wraps
 
 import logging
 lgr = logging.getLogger(__name__)
 
 from testtools import TestCase, RunTest
-from testtools.content import Content
-from testtools.content_type import ContentType
+from testtools.content import Content, text_content
+from testtools.content_type import ContentType, UTF8_TEXT
+from testtools import matchers as tm
+from testtools.matchers import Equals, Annotate, FileExists
 
-from .utils import get_test_library_paths
+from .utils import get_test_library_paths, describe_system
+#from .utils import run_command, get_shlibdeps, which, sha1sum, \
+#        get_script_interpreter, describe_system, get_test_library_paths
 from .spec import SPEC, SPECJSONEncoder
-
-console = logging.StreamHandler()
-lgr.addHandler(console)
-lgr.setLevel(logging.DEBUG)
 
 #
 # Utility code for template-based test cases
@@ -36,7 +36,7 @@ def TestArgs(*args, **kwargs):
     """Little helper to specify test arguments"""
     return (args, kwargs)
 
-def test_template(args):
+def template_case(args):
     def wrapper(func):
         func.template = args
         return func
@@ -84,12 +84,14 @@ class TemplateTestCase(type):
         attr.update(new_methods)
         return type(name, bases, attr)
 
-def discover_specs():
+def discover_specs(paths=None):
     """Helper function to discover test SPECs in configured library locations
     """
     discovered = {}
     # for all configured test library locations
-    for tld in get_test_library_paths():
+    if paths is None:
+        paths = get_test_library_paths()
+    for tld in paths:
         # for all subdirs
         for subdir in [d for d in os.listdir(tld)
                             if os.path.isdir(opj(tld, d))]:
@@ -120,26 +122,192 @@ def discover_specs():
 class TestFromSPEC(TestCase):
     __metaclass__ = TemplateTestCase
 
-    @test_template(discover_specs())
+    _system_info = None
+
+    def __init__(self, *args, **kwargs):
+        TestCase.__init__(self, *args, **kwargs)
+        self._workdir = None
+
+# derived classes should have this
+#    @template_case(discover_specs())
     def _run_spec_test(self, spec_filename):
+        wdir = self._workdir
+        # get the SPEC
         spec = SPEC(open(spec_filename))
         spec_id = spec['id']
-        print spec_id
-        self.assertEqual(49, 49)
-        #self.addDetail('input spec',
-        #               Content(ContentType('application', 'json'),
-        #                       lambda: [json.dumps(spec['inputs'],
-        #                                           cls=SPECJSONEncoder)])
-        #              )
+        details = self.getDetails()
+        ct = ContentType('application', 'json')
+        # for some strange reason doing the following in a loop doesn't work at
+        # all, all details become identical
+        details['sys'] = Content(ct, lambda: [jds(self._get_system_info())])
+        details['env'] = Content(ct, lambda: [jds(self._get_environment(spec))])
+        #os.environ['TESTKRAUT_TESTBED_PATH'] = testbed_path
+
+        # prepare the testbed, place test input into testbed
+        from .runner import prepare_local_testbed
+        prepare_local_testbed(spec, wdir, get_test_library_paths(),
+                              cachedir=None, lazy=False)
+        # execute the actual test implementation
+        self._execute_any_test_implementation(spec)
+        # check for expected output
+        self._check_output_presence(spec)
 
     def setUp(self):
         """Runs prior each test run"""
         super(TestFromSPEC, self).setUp()
-        #self._wdir = 'bla'
+        import tempfile
+        # check if we have a concurent test run
+        assert(self._workdir is None)
+        self._workdir = tempfile.mkdtemp(prefix='testkraut')
+        lgr.debug("created work dir at '%s'" % self._workdir)
 
     def tearDown(self):
         """Runs after each test run"""
         super(TestFromSPEC, self).tearDown()
-        #self._wdir = None
+        if not self._workdir is None:
+            lgr.debug("remove work dir at '%s'" % self._workdir)
+            import shutil
+            shutil.rmtree(self._workdir)
+            self._workdir = None
+
+    def _execute_any_test_implementation(self, spec):
+        type_ = spec['test']['type']
+        try:
+            test_exec = getattr(self, '_execute_%s' % type_)
+        except AttributeError:
+            raise ValueError("unsupported test type '%s'" % type_)
+        lgr.debug("run test via %s()" % test_exec.__name__)
+        ret = test_exec(spec)
+        return ret
+
+    def _execute_shell_command(self, spec):
+        import subprocess
+        testspec = spec['test']
+        cmd = testspec['command']
+        if isinstance(cmd, list):
+            # convert into a cmd string to execute via shell
+            # to get all envvar expansion ...
+            cmd = subprocess.list2cmdline(cmd)
+        # for the rest we need to execute stuff in the root of the testbed
+        initial_cwd = os.getcwdu()
+        os.chdir(self._workdir)
+        try:
+            lgr.debug("attempting to execute command '%s'" % cmd)
+            texec = subprocess.Popen(cmd,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE,
+                                    shell=True)
+            texec.wait()
+            # record the exit code
+            testspec['exitcode'] = texec.returncode
+            # store test output
+            for chan in ('stderr', 'stdout'):
+                testspec[chan] = getattr(texec, chan).read()
+                lgr.debug('%s: %s' % (chan, testspec[chan]))
+            self.assertThat(
+                texec.returncode,
+                Annotate("test shell command '%s' yielded non-zero exit code" % cmd,
+                         Equals(0)))
+        except OSError, e:
+            lgr.error("%s: %s" % (e.__class__.__name__, str(e)))
+            return False
+        finally:
+            os.chdir(initial_cwd)
+        return False
+
+    def _check_output_presence(self, spec):
+        outspec = spec.get('outputs', {})
+        unmatched_output = []
+        for ospec_id in outspec:
+            ospec = outspec[ospec_id]
+            ospectype = ospec['type']
+            if ospectype == 'file':
+                self.assertThat(
+                    ospec['value'],
+                    Annotate('expected output file missing', FileExists()))
+            elif ospectype == 'string':
+                sec, field = ospec_id.split('::')
+                self.assertThat(
+                    spec[sec][field],
+                    Annotate("unexpected output for '%s'" % ospec_id,
+                             Equals(ospec['value'])))
+            else:
+                raise NotImplementedError(
+                        "dunno how to handle output type '%s' yet"
+                        % ospectype)
+            # TODO check for file type
+
+    def _get_system_info(self):
+        if TestFromSPEC._system_info is None:
+            TestFromSPEC._system_info = describe_system()
+        return TestFromSPEC._system_info
+
+    def _get_environment(self, spec):
+        info = {}
+        for env in spec.get('environment', {}):
+            # grab envvar values
+            info[env] = os.environ.get(env, 'UNDEFINED')
+        return info
+
+    def _gather_component_info(self, spec):
+        info = {}
+        entities = {}
+        info['entities'] = entities
+        for exec_path, espec in spec.get('executables', {}).iteritems():
+            if not os.path.exists(os.path.expandvars(exec_path)):
+                # no executable? is it optional?
+                if not espec.get('optional', False):
+                    lgr.warning("failed to find required executable '%s'"
+                                % exec_path)
+                continue
+            # replace exectutable info with the full picture
+            ehash = self._describe_binary(exec_path,
+                                          entities,
+                                          type_='binary')
+            # link the old info with the new one
+            espec['entity'] = ehash
+            # check version information
+            have_version = False
+            if 'version_file' in espec:
+                verfilename = espec['version_file']
+                extract_regex = r'.*'
+                if isinstance(verfilename, list):
+                    verfilename, extract_regex = verfilename
+                # expand the filename
+                verfilename = os.path.realpath(os.path.expandvars(verfilename))
+                try:
+                    file_content = open(verfilename).read().strip()
+                    version = re.findall(extract_regex, file_content)[0]
+                    if len(version):
+                        entities[ehash]['version'] = version
+                        have_version = True
+                except:
+                    lgr.debug("failed to read version from '%s'"
+                              % verfilename)
+            if not have_version and 'version_cmd' in espec:
+                vercmd = espec['version_cmd']
+                extract_regex = r'.*'
+                if isinstance(vercmd, list):
+                    vercmd, extract_regex = vercmd
+                ret = run_command(vercmd)
+                try:
+                    # this will throw an exception if nothing is found
+                    version = re.findall(extract_regex, '\n'.join(ret['stderr']))[0]
+                    if len(version):
+                        entities[ehash]['version'] = version
+                        have_version = True
+                except:
+                    try:
+                        version = re.findall(extract_regex, '\n'.join(ret['stdout']))[0]
+                        if len(version):
+                            entities[ehash]['version'] = version
+                            have_version = True
+                    except:
+                        lgr.debug("failed to read version from '%s'" % vercmd)
+
+        if not len(entities):
+            # remove unnecessary dict
+            del info['entities']
+        return info
 
 
